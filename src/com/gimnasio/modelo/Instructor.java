217e383d14bee93e5a9b50cec7739eff58c5c1a1
nasly parra <naslytatianaparraforero3@gmail.com>
2026-02-11 16:19:12 -0500

    defini atributos como String nombre y String especialidad .Tambíen use la librería java.util.Scanner

--- src/com/gimnasio/modelo/Instructor.java
+++ src/com/gimnasio/modelo/Instructor.java
@@ -1,5 +1,37 @@
 package src.com.gimnasio.modelo;
 
 public class Instructor {
-    
+    private String nombre;
+    private String especialidad;
+
+   
+    public Instructor(String nombre, String especialidad) {
+        this.nombre = nombre;
+        this.especialidad = especialidad;
+    }
+
+ 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    @Override
+    public String toString() {
+        return "Instructor{" +
+                "nombre='" + nombre + '\'' +
+                ", especialidad='" + especialidad + '\'' +
+                '}';
+    }
 }
